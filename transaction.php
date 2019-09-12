59c045cadf7e8879eeb554644d07c338511a0d92
dragoslav <dragoslav.predojevic@procescom.com>
2019-09-12 11:24:20 +0200

    fix

--- transaction.php
+++ transaction.php
@@ -1,58 +1,52 @@
 <?php require "header.php"; ?>
-<!-- **********************************************************************************************************************************************************
-    MAIN CONTENT
-    *********************************************************************************************************************************************************** -->
-<!--main content start-->
 <section id="main-content">
     <section class="wrapper">
         <h3><i class="fa fa-angle-right"></i> Transaction </h3>
-        <!-- BASIC FORM VALIDATION -->
         <div class="row mt">
             <div class="col-lg-3 p-2">
                 <h4><i class="fa fa-angle-right"></i> Transaction Type Search</h4>
-                <div class="form-panel">
+                <div class="form-panel d-flex align-content-stretch flex-wrap" style="min-height: 290px">
                     <form role="form" class="form-horizontal style-form">
                         <div class="form-group">
-                            <label class="col-lg-4 control-label">Start : </label>
-                            <div class="col-lg-8">
+                            <label class="col-lg-5 control-label">Start : </label>
+                            <div class="col-lg-7">
                                 <input type="text" placeholder="" id="f-name" class="form-control">
                             </div>
                         </div>
                         <div class="form-group ">
-                            <label class="col-lg-4 control-label">End : </label>
-                            <div class="col-lg-8">
+                            <label class="col-lg-5 control-label">End : </label>
+                            <div class="col-lg-7">
                                 <input type="text" placeholder="" id="l-name" class="form-control">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-lg-4 control-label">Payment Type : </label>
-                            <div class="col-lg-8">
+                            <label class="col-lg-5 control-label">Payment Type : </label>
+                            <div class="col-lg-7">
                                 <input type="email" placeholder="" id="email2" class="form-control">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-lg-4 control-label">Billing Type : </label>
-                            <div class="col-lg-8">
+                            <label class="col-lg-5 control-label">Billing Type : </label>
+                            <div class="col-lg-7">
                                 <input type="email" placeholder="" id="email2" class="form-control">
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-lg-offset-3 col-lg-9">
+                            <div class="col-lg-offset-5 col-lg-7">
                                 <button class="btn btn-theme" type="submit">Reset</button>
                                 <button class="btn btn-theme04" type="button">Search</button>
                             </div>
                         </div>
                     </form>
                 </div>
-                <!-- /form-panel -->
             </div>
 
             <div class="col-lg-3 p-2">
                 <h4><i class="fa fa-angle-right"></i> Total</h4>
-                <div class="form-panel">
+                <div class="form-panel d-flex align-content-stretch flex-wrap" style="min-height: 290px">
                     <form role="form" class="form-horizontal style-form">
                         <div class="form-group">
-                            <label class="col-lg-5 control-label">GooglePlay Total : </label>
+                            <label class="col-lg-5 control-label">GooglePlay Total </label>
                             <div class="col-lg-7">
                                 <input type="text" placeholder="" id="f-name" class="form-control">
                             </div>
@@ -76,21 +70,20 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-lg-5 control-label">TOTAL PAYMENT : </label>
+                            <label class="col-lg-5 control-label"><b>TOTAL PAYMENT</b> </label>
                             <div class="col-lg-7">
                                 <input type="email" placeholder="" id="email2" class="form-control">
                             </div>
                         </div>
                     </form>
                 </div>
-                <!-- /form-panel -->
             </div>
             <div class="col-lg-3 p-2">
                 <h4><i class="fa fa-angle-right"></i> </h4>
-                <div class="form-panel">
+                <div class="form-panel d-flex align-content-stretch flex-wrap" style="min-height: 290px">
                     <form role="form" class="form-horizontal style-form">
                         <div class="form-group">
-                            <label class="col-lg-5 control-label">  Gui Total <br>: </label>
+                            <label class="col-lg-5 control-label">  Gui Total : </label>
                             <div class="col-lg-7">
                                 <input type="text" placeholder="" id="f-name" class="form-control">
                             </div>
@@ -121,14 +114,13 @@
                         </div>
                     </form>
                 </div>
-                <!-- /form-panel -->
             </div>
             <div class="col-lg-3 p-2">
                 <h4><i class="fa fa-angle-right"></i> </h4>
-                <div class="form-panel">
+                <div class="form-panel d-flex align-content-stretch flex-wrap" style="min-height: 290px">
                     <form role="form" class="form-horizontal style-form">
                         <div class="form-group">
-                            <label class="col-lg-5 control-label">Messaging Total : </label>
+                            <label class="col-lg-5 control-label">Messaging Total </label>
                             <div class="col-lg-7">
                                 <input type="text" placeholder="" id="f-name" class="form-control">
                             </div>
@@ -146,31 +138,35 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-lg-offset-3 col-lg-9">
+                            <div class="col-lg-offset-5 col-lg-7">
                                 <button class="btn btn-theme" type="submit">Export Payment</button>
                             </div>
                         </div>
                     </form>
                 </div>
-                <!-- /form-panel -->
             </div>
         </div>
         <div class="row mt">
             <div class="col-lg-12">
                 <div class="content-panel">
                     <div class="adv-table">
-                        <table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered" id="hidden-table-info">
+                        <table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered" id="transaction-table">
                             <thead>
                             <tr>
-                                <th>Created</th>
-                                <th>Expires</th>
-                                <th class="hidden-phone">ID</th>
-                                <th class="hidden-phone">Transaction</th>
-                                <th class="hidden-phone">Billing Type</th>
-                                <th class="hidden-phone">All</th>
-                                <th class="hidden-phone">Committed</th>
-                                <th class="hidden-phone">Result</th>
+                                <th>ID</th>
+                                <th>Transaction</th>
+                                <th class="hidden-phone">Billing ID</th>
+                                <th class="hidden-phone">Customer</th>
                                 <th class="hidden-phone">Meta Data</th>
+                                <th class="hidden-phone">Payment</th>
+                                <th class="hidden-phone">Start</th>
+                                <th class="hidden-phone">End</th>
+                                <th class="hidden-phone">Payment Type</th>
+                                <th class="hidden-phone">Cash</th>
+                                <th class="hidden-phone">Data</th>
+                                <th class="hidden-phone">Minute</th>
+                                <th class="hidden-phone">No.Sms</th>
+
                             </tr>
                             </thead>
                             <tbody>
@@ -184,17 +180,29 @@
                                 <td class="center hidden-phone">X</td>
                                 <td class="center hidden-phone">X</td>
                                 <td class="center hidden-phone">X</td>
+                                <td class="center hidden-phone">X</td>
+                                <td class="center hidden-phone">X</td>
+                                <td class="center hidden-phone">X</td>
+                                <td class="center hidden-phone">X</td>
                             </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
-                <!-- page end-->
             </div>
         </div>
-        </div>
-        <!-- /row -->
     </section>
-    <!-- /wrapper -->
 </section>
 <?php require "footer.php"; ?>
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        $('#transaction-table').dataTable({
+            "bFilter": false,
+            "bInfo": false,
+            "bSort": true,
+            "bProcessing": true,
+            "bLengthChange": false
+        });
+    });
+</script>
