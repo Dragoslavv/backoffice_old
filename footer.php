a6f836094a260d8f71e5781ec1a76ac166668178
dragoslav <dragoslav.predojevic@procescom.com>
2019-09-13 12:09:55 +0200

    last version

--- footer.php
+++ footer.php
@@ -2,32 +2,47 @@
 <!--footer end-->
 </section>
 <!-- js placed at the end of the document so the pages load faster -->
+<!--===============================================================================================-->
 <script src="lib/jquery/jquery.min.js"></script>
+<!--===============================================================================================-->
 <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
 <script type="text/javascript" language="javascript" src="lib/advanced-datatable/js/jquery.js"></script>
+<!--===============================================================================================-->
 <script class="include" type="text/javascript" src="lib/jquery.dcjqaccordion.2.7.js"></script>
+<!--===============================================================================================-->
 <script src="lib/jquery.scrollTo.min.js"></script>
+<!--===============================================================================================-->
 <script src="lib/jquery.nicescroll.js" type="text/javascript"></script>
+<!--===============================================================================================-->
 <script type="text/javascript" language="javascript" src="lib/advanced-datatable/js/jquery.dataTables.js"></script>
+<!--===============================================================================================-->
 <script type="text/javascript" src="lib/advanced-datatable/js/DT_bootstrap.js"></script>
+<!--===============================================================================================-->
 <!--common script for all pages-->
 <script src="lib/common-scripts.js"></script>
+<!--===============================================================================================-->
 <!--script for this page-->
 <script src="lib/jquery-ui-1.9.2.custom.min.js"></script>
+<!--===============================================================================================-->
 <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+<!--===============================================================================================-->
 <script src="lib/morris-conf.js"></script>
+<!--===============================================================================================-->
 <!--custom switch-->
 <script src="lib/bootstrap-switch.js"></script>
+<!--===============================================================================================-->
 <!--custom tagsinput-->
 <script src="lib/jquery.tagsinput.js"></script>
+<!--===============================================================================================-->
 <!--custom checkbox & radio-->
 <script type="text/javascript" src="lib/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
 <script type="text/javascript" src="lib/bootstrap-daterangepicker/date.js"></script>
 <script type="text/javascript" src="lib/bootstrap-daterangepicker/daterangepicker.js"></script>
 <script type="text/javascript" src="lib/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
 <script src="lib/form-component.js"></script>
-
+<!--===============================================================================================-->
 </body>
 
 </html>
