0757535e6670bb5c0b668960fa1ec10bfb3063b3
dragoslav <dragoslav.predojevic@procescom.com>
2020-05-12 10:54:07 +0200

    fix sim report

--- src/scenes/components/Table/sim-report-table.js
+++ src/scenes/components/Table/sim-report-table.js
@@ -53,6 +53,10 @@ export class SimReportTable extends Component{
                         data: null,
                         defaultContent:`<p><button type="button" class="btn btn-info" id="user_id_get" >Get user ID</button></p>`
                     },
+                    { title: "Voip",
+                        data: null,
+                        defaultContent:`<p><button type="button" class="btn btn-info" id="voip" >Voip</button></p>`
+                    },
                     { title: "Billing ID"},
                     { title: "Activation Time"},
                     { title: "Cash"},
@@ -122,6 +126,10 @@ export class SimReportTable extends Component{
                         { title: "User",
                             data: null,
                             defaultContent:`<p><button type="button" class="btn btn-info" id="user_id_get" >Get user ID</button></p>`
+                        },
+                        { title: "Voip",
+                            data: null,
+                            defaultContent:`<p><button type="button" class="btn btn-info" id="voip" >Voip</button></p>`
                         },
                         { title: "Billing ID"},
                         { title: "Activation Time"},
