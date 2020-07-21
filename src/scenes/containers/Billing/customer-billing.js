3de287084407004081c0c8d3a7274179b87baedd
dragoslav <dragoslav.predojevic@procescom.com>
2020-07-21 11:55:52 +0200

    select voip

--- src/scenes/containers/Billing/customer-billing.js
+++ src/scenes/containers/Billing/customer-billing.js
@@ -122,7 +122,7 @@ class CustomerBilling extends Component {
         if(sessionStorage.getItem('role') === 'ADMIN') {
 
             if( this.state.checking_voip === 'true' && this.state.select_voip !== '' ){
-console.log(this.state.select_voip);
+
                 voip_api(this.state.searchData[0].user_id,sessionStorage.getItem('username'), this.state.checking_voip, this.state.select_voip).then(result => {
 
                     if(result.status === true){
@@ -1776,7 +1776,7 @@ console.log(this.state.select_voip);
                                                 <select className={"input  true"}  onChange={this.handleChange} value={this.state.select_voip} name='select_voip'>
                                                     <option value="">Select Actions</option>
                                                     <option value="call">call</option>
-                                                    <option value="sms">sms</option>
+                                                    <option value="voice">voice</option>
                                                 </select>
                                             </div>
                                         </div>
